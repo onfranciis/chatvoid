7183de2eed2e96d75e6b7d5139ae249f45563dcf
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-11 08:34:26 +0100

    feat: add ChatScreenLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import ChatScreenLayout from "./components/ChatScreenLayout";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ChatScreenLayout />
+    </div>
+  );
 }
 
 export default App;

9185e056d7f2b8281c37a903d45f4c0b48ccaceb
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-11 08:38:00 +0100

    feat: create and style MainChat

---
+++ src/components/mainChat/MainChat.tsx
@@ -0,0 +1,15 @@
+import ChatBody from "./ChatBody";
+import Header from "./Header";
+import Input from "./Input";
+
+const MainChat = () => {
+  return (
+    <div className="MainChat">
+      <Header />
+      <ChatBody />
+      <Input />
+    </div>
+  );
+};
+
+export default MainChat;

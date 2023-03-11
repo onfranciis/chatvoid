747398a97fd4abd75331433b45b7dcbda43fa2f0
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-11 08:36:09 +0100

    feat: create and style Header

---
+++ src/components/mainChat/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="Header">
+      <p className="Name">John Doe</p>
+      <p className="Status">...typing</p>
+    </div>
+  );
+};
+
+export default Header;

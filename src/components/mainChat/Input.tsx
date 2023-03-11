d67dc95f45183809ce16034048dc5a26407d5ad5
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-11 08:37:32 +0100

    feat: create and style Input

---
+++ src/components/mainChat/Input.tsx
@@ -0,0 +1,11 @@
+const Input = () => {
+  return (
+    <div className="Input">
+      <button>Receive</button>
+      <textarea name="" id="" placeholder="Type a message..."></textarea>
+      <button>Send</button>
+    </div>
+  );
+};
+
+export default Input;

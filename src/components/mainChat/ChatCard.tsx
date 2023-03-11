516d90311dbcc16fc328abe3d3baefa9fe2ccc23
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-11 08:37:05 +0100

    feat: create and style ChatCard

---
+++ src/components/mainChat/ChatCard.tsx
@@ -0,0 +1,17 @@
+export const ChatCard1 = () => {
+  return (
+    <div className={`ChatCard1 ChatCard `}>
+      <p className="Text">Lorem Ipsum</p>
+      <p className="Time">8:42pm</p>
+    </div>
+  );
+};
+
+export const ChatCard2 = () => {
+  return (
+    <div className="ChatCard2 ChatCard">
+      <p className="Text">Lorem Ipsum</p>
+      <p className="Time">8:42pm</p>
+    </div>
+  );
+};

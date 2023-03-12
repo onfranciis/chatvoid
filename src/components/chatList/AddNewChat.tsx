8626fab7d2f268d40250b5deac648a4939318b19
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-12 09:22:12 +0100

    feat: create and style AddNewChat

---
+++ src/components/chatList/AddNewChat.tsx
@@ -0,0 +1,10 @@
+const AddNewChat = () => {
+  return (
+    <div className="AddNewChat">
+      {/* <input type="text" /> */}
+      <p>Add New User</p>
+    </div>
+  );
+};
+
+export default AddNewChat;

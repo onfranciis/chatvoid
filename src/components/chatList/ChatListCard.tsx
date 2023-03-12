521c46f7055845858a672f61b97b8daa36f1453e
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-12 09:22:56 +0100

    feat: create and style ChatListCard

---
+++ src/components/chatList/ChatListCard.tsx
@@ -0,0 +1,10 @@
+const ChatListCard = () => {
+  return (
+    <div className="ChatListCard">
+      <p className="Name">John Doe</p>
+      <p className="Message">Lorem Ipsum</p>
+    </div>
+  );
+};
+
+export default ChatListCard;

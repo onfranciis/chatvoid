1b796532754b8b25b03adad0e22575852ab423a7
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-18 15:49:30 +0100

    feat: bind ChatBody to store

--- src/components/mainChat/ChatBody.tsx
+++ src/components/mainChat/ChatBody.tsx
@@ -1,12 +1,31 @@
+import { useId } from "react";
 import { ChatCard1, ChatCard2 } from "./ChatCard";
 import Notification from "./Notification";
+import { useSelector } from "react-redux";
+import { nanoid } from "nanoid";
+import { usersStateType } from "../../redux/users/usersTypes";
 
 const ChatBody = () => {
+  const Key = useId();
+  const useSelectorUsers: usersStateType = useSelector(
+    (state: any) => state?.users
+  );
+  const chatData = useSelectorUsers.users[0];
+
   return (
     <div className="ChatBody">
-      <Notification />
-      <ChatCard1 />
-      <ChatCard2 />
+      {chatData?.messages?.map((message) => (
+        <div className="Chat">
+          <Notification key={message.day} />
+          {message?.chats?.map((chat) => (
+            <ChatCard1
+              key={`${Key}-$${nanoid()}`}
+              body={chat.message}
+              time={chat.time}
+            />
+          ))}
+        </div>
+      ))}
       <div className="EmptyBottom"></div>
     </div>
   );

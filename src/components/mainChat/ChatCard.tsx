20d2b89fbf64f1ca2df1fb2da1122ed5a65f1f69
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-18 15:50:09 +0100

    fix: make ChatCard dynamic

--- src/components/mainChat/ChatCard.tsx
+++ src/components/mainChat/ChatCard.tsx
@@ -1,17 +1,24 @@
-export const ChatCard1 = () => {
+import moment from "moment";
+
+type ChatCardPropType = {
+  body: string;
+  time: string;
+};
+
+export const ChatCard1 = ({ body, time }: ChatCardPropType) => {
   return (
     <div className={`ChatCard1 ChatCard `}>
-      <p className="Text">Lorem Ipsum</p>
-      <p className="Time">8:42pm</p>
+      <p className="Text">{body.trim()}</p>
+      <p className="Time">{moment(JSON.parse(time)).format("h:mma")}</p>
     </div>
   );
 };
 
-export const ChatCard2 = () => {
+export const ChatCard2 = ({ body, time }: ChatCardPropType) => {
   return (
-    <div className="ChatCard2 ChatCard">
-      <p className="Text">Lorem Ipsum</p>
-      <p className="Time">8:42pm</p>
+    <div className={`ChatCard1 ChatCard `}>
+      <p className="Text">{body.trim()}</p>
+      <p className="Time">{moment(JSON.parse(time)).format("h:mma")}</p>
     </div>
   );
 };

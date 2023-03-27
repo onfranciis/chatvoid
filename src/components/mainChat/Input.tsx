aaabc68cb4b9179fb322620310e23ca54a5a6339
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-27 15:50:26 +0100

    feat: bind Input to store

--- src/components/mainChat/Input.tsx
+++ src/components/mainChat/Input.tsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import { ADD_NEW_USER, NEW_CHAT, SAME_DAY } from "../../redux/users/usersSlice";
+import { useDispatch } from "react-redux";
+import moment from "moment";
+import { nanoid } from "nanoid";
+
 const Input = () => {
+  const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+
+  const dispatchBody = {
+    id: nanoid(),
+    message,
+    time: JSON.stringify(moment()),
+    type: 1,
+  };
+
+  const handleSend = () => {
+    dispatch(ADD_NEW_USER(dispatchBody));
+  };
+
+  const handleSend2 = () => {
+    dispatch(SAME_DAY(dispatchBody));
+  };
+
   return (
     <div className="Input">
-      <button>Receive</button>
-      <textarea name="" id="" placeholder="Type a message..."></textarea>
-      <button>Send</button>
+      <button onClick={handleSend2}>Receive</button>
+
+      <textarea
+        name="message"
+        id="message"
+        placeholder="Type a message..."
+        value={message}
+        onChange={(e) => setMessage(e.currentTarget.value)}
+      ></textarea>
+
+      <button onClick={handleSend}>Send</button>
     </div>
   );
 };

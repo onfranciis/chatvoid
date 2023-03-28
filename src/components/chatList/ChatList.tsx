dce013e529a4e28f0e6f5316252c856aa419202c
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-28 18:51:57 +0100

    feat: bind ChatList to store

--- src/components/chatList/ChatList.tsx
+++ src/components/chatList/ChatList.tsx
@@ -1,11 +1,29 @@
+import { useSelector } from "react-redux";
 import AddNewChat from "./AddNewChat";
 import ChatListCard from "./ChatListCard";
+import store from "../../redux/store";
+import { nanoid } from "nanoid";
+const { getState } = store;
 
 const ChatList = () => {
+  const Chats = useSelector(
+    (state: ReturnType<typeof getState>) => state?.users.users
+  );
+
   return (
     <div className="ChatList">
       <AddNewChat />
-      <ChatListCard />
+      {Chats.map((chat) => (
+        <ChatListCard
+          key={nanoid()}
+          Name={chat.username}
+          LastMessage={
+            chat?.messages[chat.messages.length - 1]?.chats[
+              chat.messages[chat.messages.length - 1].chats.length - 1
+            ]?.message
+          }
+        />
+      ))}
     </div>
   );
 };

f404b1fa962d3cf3ea1d28c551b26e0c0c9c4c76
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-28 18:49:42 +0100

    feat: bind dispatch to AddNewChat

--- src/components/chatList/AddNewChat.tsx
+++ src/components/chatList/AddNewChat.tsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { ADD_NEW_USER } from "../../redux/users/usersSlice";
+import { nanoid } from "nanoid";
+import moment from "moment";
+
 const AddNewChat = () => {
+  const [userName, setUserName] = useState<string>("New User");
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const dispatch = useDispatch();
+
+  const dispatchBody = {
+    id: nanoid(),
+    message: userName,
+    time: JSON.stringify(moment()),
+    type: 1,
+  };
+
+  const handleSubmit = () => {
+    setEditMode(false);
+    dispatch(ADD_NEW_USER(dispatchBody));
+    setUserName("New User");
+  };
+
   return (
     <div className="AddNewChat">
-      {/* <input type="text" /> */}
-      <p>Add New User</p>
+      {editMode ? (
+        <input
+          type="text"
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.currentTarget.value);
+          }}
+          autoFocus
+          onBlur={handleSubmit}
+        />
+      ) : (
+        <p
+          onClick={() => {
+            setEditMode(true);
+          }}
+        >
+          Add New User
+        </p>
+      )}
     </div>
   );
 };

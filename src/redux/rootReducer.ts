3a4c1955acc202b2db1972440028cfa146ba2491
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-29 20:30:57 +0100

    feat: add util to redux store

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import usersReducer from "./users/usersSlice";
+import utilReducer from "./util/utilSlice";
 
 const rootReducer = combineReducers({
   users: usersReducer,
+  util: utilReducer,
 });
 
 export default rootReducer;

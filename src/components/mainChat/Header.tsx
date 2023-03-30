427addbca1a1a1a1fa23a32b7a9766fd69b3a246
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-30 20:34:07 +0100

    feat: bind Header to store

--- src/components/mainChat/Header.tsx
+++ src/components/mainChat/Header.tsx
@@ -1,8 +1,22 @@
+import { useSelector } from "react-redux";
+import store from "../../redux/store";
+const { getState } = store;
+
 const Header = () => {
+  const selectedMessageIndex = useSelector(
+    (state: ReturnType<typeof getState>) => state.util.selectedMessage
+  );
+
+  const selectedMessage = useSelector((state: ReturnType<typeof getState>) =>
+    selectedMessageIndex !== null
+      ? state?.users.users[selectedMessageIndex]
+      : null
+  );
+
   return (
     <div className="Header">
-      <p className="Name">John Doe</p>
-      <p className="Status">...typing</p>
+      <p className="Name">{selectedMessage?.username}</p>
+      <p className="Status">{selectedMessage?.typing}</p>
     </div>
   );
 };

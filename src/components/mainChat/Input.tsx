e628d9f8ffe8ed7713516f4abad807aa8e71f442
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-04-02 00:12:38 +0100

    feat: update the handleSend function
    - Trigger input focus when selectedMessageIndex changes
    - Call handleSend when user presses Enter key while on the input

--- src/components/mainChat/Input.tsx
+++ src/components/mainChat/Input.tsx
@@ -1,27 +1,88 @@
-import { useState } from "react";
-import { ADD_NEW_USER, NEW_CHAT, SAME_DAY } from "../../redux/users/usersSlice";
-import { useDispatch } from "react-redux";
+import { KeyboardEvent, useEffect, useRef, useState } from "react";
+import { NEW_CHAT, SAME_DAY } from "../../redux/users/usersSlice";
+import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 import { nanoid } from "nanoid";
+import store from "../../redux/store";
+const { getState } = store;
 
 const Input = () => {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState<string>("");
   const dispatch = useDispatch();
+  const TextAreaRef = useRef<HTMLTextAreaElement>(null);
 
-  const dispatchBody = {
+  const selectedMessageIndex = useSelector(
+    (state: ReturnType<typeof getState>) => state?.util?.selectedMessage
+  );
+
+  const getCurrentChatDay = useSelector(
+    (state: ReturnType<typeof getState>) =>
+      state?.users.users[selectedMessageIndex ?? 0]?.messages.slice(-1)[0]?.day
+  );
+
+  const parsedCurrentChatDay =
+    getCurrentChatDay !== undefined ? JSON.parse(getCurrentChatDay) : "";
+
+  const dispatchBody = (type: number) => ({
     id: nanoid(),
     message,
     time: JSON.stringify(moment()),
-    type: 1,
-  };
+    type: type,
+  });
 
-  const handleSend = () => {
-    dispatch(SAME_DAY(dispatchBody));
+  /**
+   * Handle Send
+   * @param {number} type
+   * @returns {void}
+   * Call different dispatch actions depending on the day the handleSend function was called.
+   * If the funtion was called on the same day the last message was sent,
+   * it would call SAME_DAY action and if it wasn't,
+   * it would NEW_CHAT action.
+   */
+
+  const handleSend = (type: number): void => {
+    if (selectedMessageIndex !== null) {
+      if (
+        moment(parsedCurrentChatDay).format("YYYY-MM-DD") ==
+        moment().format("YYYY-MM-DD")
+      ) {
+        dispatch(
+          SAME_DAY({
+            mainData: dispatchBody(type),
+            selectedMessageIndex: selectedMessageIndex,
+          })
+        );
+      } else {
+        dispatch(
+          NEW_CHAT({
+            mainData: dispatchBody(type),
+            selectedMessageIndex: selectedMessageIndex,
+          })
+        );
+      }
+    } else {
+    }
+
+    setMessage("");
   };
 
   const handleSend2 = () => {
-    dispatch(NEW_CHAT(dispatchBody));
+    handleSend(1);
   };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key == "Enter") {
+      if (e.key == "Enter" && e.shiftKey) {
+      } else {
+        e.preventDefault();
+        handleSend(2);
+      }
+    }
+  };
+
+  useEffect(() => {
+    TextAreaRef.current?.focus();
+  }, [selectedMessageIndex]);
 
   return (
     <div className="Input">
@@ -33,9 +94,11 @@ const Input = () => {
         placeholder="Type a message..."
         value={message}
         onChange={(e) => setMessage(e.currentTarget.value)}
+        onKeyDown={handleKeyDown}
+        ref={TextAreaRef}
       ></textarea>
 
-      <button onClick={handleSend}>Send</button>
+      <button onClick={() => handleSend(2)}>Send</button>
     </div>
   );
 };

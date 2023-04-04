9dd072bf26c8be6a2651d68b0f8dd9dd48618434
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-04-04 19:50:22 +0100

    feat: create and style EmptyUser

---
+++ src/components/mainChat/EmptyUser.tsx
@@ -0,0 +1,14 @@
+const EmptyUser = () => {
+  return (
+    <div className="EmptyUser">
+      <div className="Blob">
+        <img src="/Astronaut.svg" alt="" />
+      </div>
+      <p>
+        Click on <span>Add New User</span> to begin
+      </p>
+    </div>
+  );
+};
+
+export default EmptyUser;

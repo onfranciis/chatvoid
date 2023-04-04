ce0e93cd5c8738d814e53e0e03e9abf09a77a0fc
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-04-04 19:59:01 +0100

    feat: add support for unselected chat

--- src/components/mainChat/MainChat.tsx
+++ src/components/mainChat/MainChat.tsx
@@ -1,9 +1,19 @@
+import { useSelector } from "react-redux";
 import ChatBody from "./ChatBody";
 import Header from "./Header";
 import Input from "./Input";
+import store from "../../redux/store";
+import EmptyUser from "./EmptyUser";
+const { getState } = store;
 
 const MainChat = () => {
-  return (
+  const selectedMessage = useSelector(
+    (state: ReturnType<typeof getState>) => state?.util.selectedMessage === null
+  );
+
+  return selectedMessage ? (
+    <EmptyUser />
+  ) : (
     <div className="MainChat">
       <Header />
       <ChatBody />

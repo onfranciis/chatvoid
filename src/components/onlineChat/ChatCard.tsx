67e7e44e66a78ee796faa22a5449fcb5f60b71c2
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-07-06 16:26:26 +0100

    feat: create online ChatCard

---
+++ src/components/onlineChat/ChatCard.tsx
@@ -0,0 +1,25 @@
+import moment from "moment";
+
+type ChatCardPropType = {
+  body: string;
+  time: string;
+};
+
+export const ChatCard1 = ({ body, time }: ChatCardPropType) => {
+  return (
+    <div className={`ChatCard ChatCard1 `}>
+      <p className="Name">John Doe</p>
+      <p className="Text">{body.trim()}</p>
+      <p className="Time">{moment(JSON.parse(time)).format("h:mma")}</p>
+    </div>
+  );
+};
+
+export const ChatCard2 = ({ body, time }: ChatCardPropType) => {
+  return (
+    <div className={`ChatCard ChatCard2 `}>
+      <p className="Text">{body.trim()}</p>
+      <p className="Time">{moment(JSON.parse(time)).format("h:mma")}</p>
+    </div>
+  );
+};

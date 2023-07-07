221d0d9b6d916abec8499d4744680f5474c284b6
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-07-07 01:19:13 +0100

    feat: add mode to utilSlice and update types

--- src/redux/util/utilSlice.ts
+++ src/redux/util/utilSlice.ts
@@ -3,13 +3,19 @@ import { utilActionType, utilStateType } from "./utilTypes";
 
 export const utilSlice = createSlice({
   name: "util",
-  initialState: <utilStateType>{ selectedMessage: null },
+  initialState: <utilStateType>{ selectedMessage: null, mode: 0 },
   reducers: {
     SET_SELECTED_MESSAGE: (state, action: utilActionType) => {
-      state.selectedMessage = action.payload;
+      state.selectedMessage = action.payload.selectedMessagePayload;
+      state.mode = 0;
+    },
+
+    SET_ONLINE: (state) => {
+      state.mode = 1;
+      state.selectedMessage = null;
     },
   },
 });
 
-export const { SET_SELECTED_MESSAGE } = utilSlice.actions;
+export const { SET_SELECTED_MESSAGE, SET_ONLINE } = utilSlice.actions;
 export default utilSlice.reducer;
--- src/redux/util/utilTypes.ts
+++ src/redux/util/utilTypes.ts
@@ -1,7 +1,16 @@
+// Mode has three values, 0 ,1 and null
+// 0 switches to offline
+// 1 switches to online
+// null clears the chat display
+
 export interface utilStateType {
   selectedMessage: number | null;
+  mode: 0 | 1 | null;
 }
 
 export interface utilActionType {
-  payload: number | null;
+  payload: {
+    selectedMessagePayload: number | null;
+    modePayload: null | 0 | 1;
+  };
 }
